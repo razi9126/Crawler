7d3afc96591e7284647dec7f8f3fa80ad753c3f7
razi <razi9126@gmail.com>
2018-05-29 22:33:07 +0500

    Updated

--- request.py
+++ request.py
@@ -1,8 +1,9 @@
 import requests
 
-# prints the record which have the foodpandaUrl field.
-
-
+# prints the records which have the foodpandaUrl field.
+# Now need to alter menu.py so that it takes this url as a commandline argument. then
+# returns the json object array to this program. This program would then iterate over all the fields 
+# //
 r = requests.get("https://api.paitoo.com.pk/restaurants/all")
 data = r.json()
 
